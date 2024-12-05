3376d9c0926be52acceaea121f8360fa69fc7aea
Oleksandr Balan <alex.balan.wewox@gmail.com>
2024-12-05 08:05:42 +0100

    Add day 1 solution

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,21 +1,39 @@
+import kotlin.math.abs
+
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
-    }
-
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
-
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
-
-    // Or read a large test input from the `src/Day01_test.txt` file:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
-    // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
-    part1(input).println()
-    part2(input).println()
+
+    val data = Day01.parse(input)
+    println(Day01.part1(data))
+    println(Day01.part2(data))
+}
+
+private object Day01 {
+
+    fun parse(input: List<String>): Locations {
+        val locations = input.map {
+            val pair = it.split("   ")
+            pair[0].toInt() to pair[1].toInt()
+        }
+
+        return Locations(
+            first = locations.map { it.first },
+            second = locations.map { it.second },
+        )
+    }
+
+    fun part1(locations: Locations): Int =
+        locations.first.sorted().zip(locations.second.sorted())
+            .sumOf { pair -> abs(pair.first - pair.second) }
+
+    fun part2(locations: Locations): Int =
+        locations.first.sumOf { location ->
+            val times = locations.second.count { it == location }
+            location * times
+        }
+
+    data class Locations(
+        val first: List<Int>,
+        val second: List<Int>,
+    )
 }
